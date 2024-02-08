dd16011526d4a284b5cfa31aaacc97a188b5e9bb
tgonati327 <tgonati327@g.educaand.es>
2024-02-08 15:14:21 +0100

    -

---
+++ demosjQuery/demosjQuery/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+	$('#botona').click(function(){$('#capa').html('Has hecho clic en A')});
+
+	$('#botonb').click(function(){$('#capa').html('Has hecho clic en B')});
+
+})
+
